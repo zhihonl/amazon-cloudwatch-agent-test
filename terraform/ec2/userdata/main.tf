1e337a3cab047da624f24443f9536a3345f90c96
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 17:33:24 -0400

    Remove comments

--- terraform/ec2/userdata/main.tf
+++ terraform/ec2/userdata/main.tf
@@ -65,20 +65,6 @@ resource "null_resource" "integration_test" {
     host        = aws_instance.cwagent.public_ip
   }
 
-  # Prepare Integration Test
-/*   provisioner "remote-exec" {
-    inline = [
-      "echo sha ${var.cwa_github_sha}",
-      "cloud-init status --wait",
-      "echo clone and install agent",
-      "git clone --branch ${var.github_test_repo_branch} ${var.github_test_repo}",
-      "cd amazon-cloudwatch-agent-test",
-      "aws s3 cp s3://${local.binary_uri} .",
-      "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
-      var.install_agent,
-    ]
-  } */
-
   #Run sanity check and integration test
   provisioner "remote-exec" {
     inline = [
