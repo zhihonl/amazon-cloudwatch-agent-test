0ea9448bf77e4660c9add14ac6c7edb115dbdcdb
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 17:15:09 -0400

    Remove go paths

--- terraform/ec2/userdata/install_agent.sh
+++ terraform/ec2/userdata/install_agent.sh
@@ -5,8 +5,5 @@ cd /home/ec2-user/
 git clone --branch ${github_test_repo_branch} ${github_test_repo}
 cd amazon-cloudwatch-agent-test
 aws s3 cp s3://${binary_uri} .
-export HOME=/root
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
 rpm -U ./amazon-cloudwatch-agent.rpm
 cloud-init status --wait
