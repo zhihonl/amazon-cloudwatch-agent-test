847bbbc4f16a59fc2a910c76cf6cf6020e343e21
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 01:51:11 -0400

    Fix path for s3

--- terraform/ec2/userdata/install_agent.sh
+++ terraform/ec2/userdata/install_agent.sh
@@ -4,7 +4,7 @@ echo Script is executing
 echo clone and install agent
 git clone --branch ${github_test_repo_branch} ${github_test_repo}
 cd amazon-cloudwatch-agent-test
-aws s3 cp s3://${binary_uri}
+aws s3 cp s3://${binary_uri} .
 export PATH=$PATH:/snap/bin:/usr/local/go/bin
 ${install_agent}
 cloud-init status --wait
