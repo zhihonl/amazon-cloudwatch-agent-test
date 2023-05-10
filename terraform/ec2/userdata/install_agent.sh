7b94d3d0422257f6a3cd5db86507ca04f4a53e5e
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 02:11:06 -0400

    User ec2-user as download path

--- terraform/ec2/userdata/install_agent.sh
+++ terraform/ec2/userdata/install_agent.sh
@@ -2,6 +2,7 @@
 echo sha ${cwa_github_sha}
 echo Script is executing
 echo clone and install agent
+cd /home/ec2-user/
 git clone --branch ${github_test_repo_branch} ${github_test_repo}
 cd amazon-cloudwatch-agent-test
 aws s3 cp s3://${binary_uri} .
