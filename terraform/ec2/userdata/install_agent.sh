02e1abeb6b0c1237b7477e0639058b5c5b924a1b
Zhihong Lin <zhiholin@amazon.com>
2023-05-10 01:45:27 -0400

    Cloud init wait after all executions

--- terraform/ec2/userdata/install_agent.sh
+++ terraform/ec2/userdata/install_agent.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo sha ${cwa_github_sha}
 echo Script is executing
-cloud-init status --wait
 echo clone and install agent
 git clone --branch ${github_test_repo_branch} ${github_test_repo}
 cd amazon-cloudwatch-agent-test
 aws s3 cp s3://${binary_uri}
 export PATH=$PATH:/snap/bin:/usr/local/go/bin
 ${install_agent}
+cloud-init status --wait
