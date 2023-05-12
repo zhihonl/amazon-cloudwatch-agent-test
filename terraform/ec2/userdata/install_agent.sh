e70eaaeee9c11dfd069cd2a7056c6e9059f0325b
Zhihong Lin <zhiholin@amazon.com>
2023-05-12 11:12:50 -0400

    CD into test path

--- terraform/ec2/userdata/install_agent.sh
+++ terraform/ec2/userdata/install_agent.sh
@@ -10,5 +10,6 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 echo Running command ${install_agent}
 ${install_agent}
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/amazon-cloudwatch-agent-test/test/userdata/agent_configs/cpu_config.json
+cd /home/ec2-user/amazon-cloudwatch-agent-test/test/userdata/agent_configs/
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:cpu_config.json
 cloud-init status --wait
