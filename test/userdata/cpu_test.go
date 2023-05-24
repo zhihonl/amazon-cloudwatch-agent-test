83fa05fcb10859c13313ec467f9c73b24a2b4877
Zhihong Lin <zhiholin@amazon.com>
2023-05-24 00:28:15 -0400

    Fix pointer

--- test/userdata/cpu_test.go
+++ test/userdata/cpu_test.go
@@ -49,7 +49,7 @@ func (t *CPUTestRunner) GetMeasuredMetrics() []string {
 		"cpu_time_active_userdata"}
 }
 
-func (t *CPUTestRunner) RunAgent(T test_runner.TestRunner) (status.TestGroupResult, error) {
+func (t *CPUTestRunner) RunAgent(T *test_runner.TestRunner) (status.TestGroupResult, error) {
 	log.Printf("Running test for runAgent in userdata mode")
 	testGroupResult := status.TestGroupResult{
 		Name: t.GetTestName(),
