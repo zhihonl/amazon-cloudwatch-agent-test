11f38b62afe846ba750c02ba161f32df4dd40468
Zhihong Lin <zhiholin@amazon.com>
2023-08-07 20:18:46 -0400

    Check task metadat

--- terraform/ecs_ec2/daemon/main.tf
+++ terraform/ecs_ec2/daemon/main.tf
@@ -239,7 +239,7 @@ resource "null_resource" "validator" {
   provisioner "local-exec" {
     command = <<-EOT
       echo Checking task metadata
-      curl -s http://localhost:51678/v1/metadata
+      curl http://localhost:51678/v1/metadata
       echo "Validating metrics/logs"
       cd ../../..
       go test ${var.test_dir} -timeout 0 -computeType=ECS -ecsLaunchType=EC2 -ecsDeploymentStrategy=DAEMON -cwagentConfigSsmParamName=${local.cwagent_config_ssm_param_name} -clusterArn=${aws_ecs_cluster.cluster.arn} -cwagentECSServiceName=${aws_ecs_service.cwagent_service.name} -v
